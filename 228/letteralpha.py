a3c43eda8d7e3c39e68b54a5f22a06078aed2cf5
feedme2 <pham.alan10@gmail.com>
2015-09-17 20:33:34 -0700

    Finished easy 228.

--- 228/letteralpha.py
+++ 228/letteralpha.py
@@ -1,6 +1,19 @@
-# Open input.txt for read only.
+
+# Open input.txt and read in all the lines in an array.
 inFile = open('input.txt','r')
-line = inFile.readline()
-if (''.join(sorted(line)) == line):
-    print "YES"
-#print(if (''.join(sorted(line)) == line))
+lines = inFile.readlines()
+
+#for loop.
+for x in range(0, len(lines)):
+    #get line length.
+    lineLength = len(lines[x])
+
+    '''
+        sort the lines[x], which gives an array so join together. 
+        Exclude the new line element by taking the last lineLength-1 elements.
+        Compare to lines[x]
+    '''
+    if ''.join(sorted(lines[x])[-(lineLength-1):]) == lines[x][:lineLength-1]:
+        print (lines[x][:lineLength-1]+ " IN ORDER")
+    else:
+        print (lines[x][:lineLength-1]+ " NOT IN ORDER")
